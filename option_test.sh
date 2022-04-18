fdf83460a92066cfa8ee6e0e5d9ba894e232f8b2
intercisa <sipiczkibarna@gmail.com>
2022-04-18 20:19:13 +0200

    init

---
+++ option_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#https://linuxconfig.org/bash-script-flags-usage-with-arguments-examples
+
+while getopts 'lha:' OPTION; do
+	case "$OPTION" in
+		l)
+			echo "option l"
+			;;
+		h)
+			echo "option h"
+			;;
+		a)
+			avalue="$OPTARG"
+			echo "the value provided is $OPTARG"
+			;;
+		?)
+			echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
+			exit 1
+			;;
+	esac
+done
+shift "$(($OPTIND -1))"
